0db0f75ce7727c205da09bea3ca6a7a5bc4433a1
Vishal Lama <vishal.lama@gmail.com>
2016-10-07 19:43:05 -0600

    Do Ex 2: Concatenate strings

---
+++ code/I_Fixed-Size_Data/01_Arithmetic/ex_002.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex_002) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+;; Exercise 2
+
+; define a prefix and a suffix
+(define prefix "hello")
+(define suffix "world")
+
+; concatenate the prefix and the suffix while
+; inserting an underscore (_) between them
+(string-append prefix "_" suffix)
